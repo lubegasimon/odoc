69cbd5adf9e845ae0ce1c35e0e2aadf07b9aa0cc
lubegasimon <lubegasimon73@gmail.com>
2021-06-03 07:33:06 +0300

    draft

--- test/generators/cases_pre408/gen_link/gen_link.ml
+++ test/generators/cases_pre408/gen_link/gen_link.ml
@@ -1,9 +1,3 @@
 let () =
-  let paths =
-    Gen_link_lib.read_file_from_dir (Fpath.filename Gen_link_lib.cases)
-  in
-  let paths =
-    List.filter (fun p -> not (Gen_link_lib.is_dot_ocamlformat p)) paths
-  in
-  let stanzas = Gen_link_lib.gen_rule paths "4.10" in
+  let stanzas = Gen_link_lib.gen_rule "4.10" in
   List.iter (Sexplib0.Sexp.pp Format.std_formatter) stanzas
--- test/generators/gen_link_lib.ml
+++ test/generators/gen_link_lib.ml
@@ -1,139 +1,40 @@
 type sexp = Sexplib0.Sexp.t = Atom of string | List of sexp list
 
-let cu_target_rule dep_path target_path =
-  List
-    [
-      Atom "rule";
-      List [ Atom "target"; Atom target_path ];
-      List [ Atom "deps"; Atom (Fpath.to_string dep_path) ];
-      List
-        [
-          Atom "action";
-          List
-            [
-              Atom "run";
-              Atom "ocamlc";
-              Atom "-c";
-              Atom "-bin-annot";
-              Atom "-o";
-              Atom "%{target}";
-              Atom "%{deps}";
-            ];
-        ];
-    ]
+let read_file_from_dir dir =
+  let filenames =
+    let arr = Sys.readdir dir in
+    Array.sort String.compare arr;
+    Array.to_list arr
+  in
+  let dir = Fpath.v dir in
+  List.map (Fpath.( / ) dir) filenames
 
-let odoc_target_rule dep_path target_path =
-  List
-    [
-      Atom "rule";
-      List [ Atom "target"; Atom (Fpath.basename target_path) ];
-      List [ Atom "deps"; Atom (Fpath.basename dep_path) ];
-      List
-        [
-          Atom "action";
-          List
-            [
-              Atom "run";
-              Atom "odoc";
-              Atom "compile";
-              Atom "--pkg";
-              Atom "test";
-              Atom "-o";
-              Atom "%{target}";
-              Atom "%{deps}";
-            ];
-        ];
-    ]
+let cases = Fpath.v "cases"
 
-let odocl_target_rule dep_path target_path =
-  List
-    [
-      Atom "rule";
-      List [ Atom "target"; Atom (Fpath.basename target_path) ];
-      List [ Atom "deps"; Atom (Fpath.basename dep_path) ];
-      List
-        [
-          Atom "action";
-          List
-            [
-              Atom "run";
-              Atom "odoc";
-              Atom "link";
-              Atom "-o";
-              Atom "%{target}";
-              Atom "%{deps}";
-            ];
-        ];
-    ]
+let all_compile_files = read_file_from_dir (Fpath.filename cases)
 
-let mld_odoc_target_rule dep_path target_path =
-  List
-    [
-      Atom "rule";
-      List [ Atom "target"; Atom (Fpath.basename target_path) ];
-      List [ Atom "deps"; Atom (Fpath.to_string dep_path) ];
-      List
-        [
-          Atom "action";
-          List
-            [
-              Atom "run";
-              Atom "odoc";
-              Atom "compile";
-              Atom "--pkg";
-              Atom "test";
-              Atom "-o";
-              Atom "%{target}";
-              Atom "%{deps}";
-            ];
-        ];
-    ]
+let foo_files =
+  Gen_backend.lines_of_command "dune exec compile "
+    [ String.concat " " (List.map Fpath.to_string all_compile_files) ]
+
+let filtered_odocl_files =
+  List.filter
+    (fun f ->
+      match String.split_on_char '.' f with
+      | [ _; "odocl" ] -> true
+      | _ :: _ -> false
+      | [] -> false)
+    foo_files
 
 let set_odocl_ext = Fpath.set_ext ".odocl"
 
 let set_odoc_ext = Fpath.set_ext ".odoc"
 
-let file_rule path ext =
-  let cm_file = Fpath.set_ext ext path in
-  let odoc_file = set_odoc_ext path in
-  let odocl_file = set_odocl_ext path in
-  [
-    cu_target_rule path (Fpath.basename cm_file);
-    odoc_target_rule cm_file odoc_file;
-    odocl_target_rule odoc_file odocl_file;
-  ]
-
-let mld_file_rule path =
-  let path' = Fpath.(v ("page-" ^ basename path)) in
-  let odoc_file = set_odoc_ext path' in
-  let odocl_file = set_odocl_ext path' in
-  [
-    mld_odoc_target_rule path odoc_file; odocl_target_rule odoc_file odocl_file;
-  ]
-
-let die s =
-  prerr_endline s;
-  exit 1
-
 let path' () f = Filename.quote (Fpath.to_string f)
 
 let ext' () f = Filename.quote (Fpath.get_ext f)
 
-let cases = Fpath.v "cases"
-
 let is_dot_ocamlformat p = Fpath.filename p = ".ocamlformat"
-
-let gen_rule_for_source_file path =
-  let ext = Fpath.get_ext path in
-  match ext with
-  | ".ml" -> file_rule path ".cmt"
-  | ".mli" -> file_rule path ".cmti"
-  | ".mld" -> mld_file_rule path
-  | _ ->
-      die
-        (Printf.sprintf
-           "Don't know what to do with %a because of unrecognized %a extension."
-           path' path ext' path)
 
 let html, latex, man = ("html", "latex", "man")
 
@@ -183,13 +84,13 @@ let backends = [ html; latex; man ]
 
 let dep_atom p = Atom (Printf.sprintf "%%{dep:%s}" (Fpath.to_string p))
 
-let odocls backend paths =
-  paths
+let odocls backend =
+  List.map Fpath.v filtered_odocl_files
   |> List.map (fun p ->
          let path = Fpath.relativize ~root:backend p in
          match path with Some p -> dep_atom p | None -> assert false)
 
-let gen_backend_diff_rule paths =
+let gen_backend_diff_rule =
   List.map
     (fun b ->
       List
@@ -206,8 +107,7 @@ let gen_backend_diff_rule paths =
                   List
                     [
                       Atom "pipe-stdout";
-                      List
-                        (Atom "run" :: Atom b.gen_exe :: odocls b.subdir paths);
+                      List (Atom "run" :: Atom b.gen_exe :: odocls b.subdir);
                       List [ Atom "run"; Atom "dune"; Atom "format-dune-file" ];
                     ];
                 ];
@@ -235,33 +135,7 @@ let diff_rules ocaml_ver =
         ])
     backends
 
-let gen_backend_rule paths ocaml_ver =
-  [ gen_backend_diff_rule paths; diff_rules ocaml_ver ] |> List.flatten
+let gen_backend_rule ocaml_ver =
+  [ gen_backend_diff_rule; diff_rules ocaml_ver ] |> List.flatten
 
-let read_file_from_dir dir =
-  let filenames =
-    let arr = Sys.readdir dir in
-    Array.sort String.compare arr;
-    Array.to_list arr
-  in
-  let dir = Fpath.v dir in
-  List.map (Fpath.( / ) dir) filenames
-
-let gen_rule paths ocaml_ver =
-  let paths' =
-    List.map
-      (fun p ->
-        let path = Fpath.relativize ~root:cases p in
-        match path with
-        | Some p ->
-            if Fpath.get_ext p = ".mld" then
-              set_odocl_ext Fpath.(parent p / ("page-" ^ filename p))
-            else set_odocl_ext Fpath.(parent p / filename p)
-        | None -> assert false)
-      paths
-  in
-  List.flatten
-    [
-      List.(flatten (map gen_rule_for_source_file paths));
-      gen_backend_rule paths' ocaml_ver;
-    ]
+let gen_rule ocaml_ver = List.flatten [ gen_backend_rule ocaml_ver ]
