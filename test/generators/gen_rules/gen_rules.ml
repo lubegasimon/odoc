75c531f98c3fc130fbfe9b76e06467dafb46ee5e
lubegasimon <lubegasimon73@gmail.com>
2021-07-04 16:46:02 +0300

    draft

--- test/generators/gen_rules/gen_rules.ml
+++ test/generators/gen_rules/gen_rules.ml
@@ -21,48 +21,6 @@ let html_target_rule path : Gen_rules_lib.sexp =
         ];
     ]
 
-let latex_target_rule path : Gen_rules_lib.sexp =
-  List
-    [
-      Atom "action";
-      List
-        [
-          Atom "progn";
-          List
-            [
-              Atom "run";
-              Atom "odoc";
-              Atom "latex-generate";
-              Atom "-o";
-              Atom ".";
-              Atom "--extra-suffix";
-              Atom "gen";
-              Atom ("%{dep:" ^ Fpath.to_string path ^ "}");
-            ];
-        ];
-    ]
-
-let man_target_rule path : Gen_rules_lib.sexp =
-  List
-    [
-      Atom "action";
-      List
-        [
-          Atom "progn";
-          List
-            [
-              Atom "run";
-              Atom "odoc";
-              Atom "man-generate";
-              Atom "-o";
-              Atom ".";
-              Atom "--extra-suffix";
-              Atom "gen";
-              Atom ("%{dep:" ^ Fpath.to_string path ^ "}");
-            ];
-        ];
-    ]
-
 let read_file_from_dir dir =
   let filenames =
     let arr = Sys.readdir dir in
@@ -79,11 +37,7 @@ let () =
   in
   let stanzas =
     Gen_rules_lib.gen_rule
-      [
-        (html_target_rule, Fpath.v "html", Some "--flat");
-        (latex_target_rule, Fpath.v "latex", None);
-        (man_target_rule, Fpath.v "man", None);
-      ]
+      [ (html_target_rule, Fpath.v "html", Some "--flat") ]
       paths
   in
   List.iter (Sexplib0.Sexp.pp Format.std_formatter) stanzas
